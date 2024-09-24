b42f87ab2d662f55b428fdaad907fbe4a15ce14e
Mahendra <mahendrapurohit9889@gmail.com>
2024-09-24 18:54:55 +0530

    mySql

--- Mysql.sql
+++ Mysql.sql
@@ -51,3 +51,165 @@ DML : Data Manipulation Language
 DCL : Data Control Language
 
 TCL : Transaction Control Language  
+
+
+
+--  // PRIMARY KEY
+-  It Always a Unique And Not Null 
+- a primary Key is only one in One table 
+
+
+-- // FOREIGN KEY
+- Foreign key are multiple in one table 
+- Foreign key is refers primary key of onother table 
+- Foreign key can have duplicate & null values
+- link between two tables
+
+
+--   // DEFAULT 
+   -  sets values defaulty when data not entered ;
+
+
+--   // Constraints
+
+     // CHEK - it can limit the values Allowed in a column
+
+ 
+ --   //  DISTINCT  
+
+    -> gives uniq value from column 
+    -> No repate 
+
+
+    -- Where Clause
+       -> to define some condition
+
+      SELECT * FROM student WHERE marks > 80;
+      SELECT * FROM Employee WHERE  
+
+    --    // Oparator 
+
+     Arithmatic Operators : + (additon),-(subtraction),*(multiplication),/(devision),%(modulus) 
+     Coparison Operators : = (equal to),!=(not Equal to ),>, >=, <, <=
+     Logical Operators : AND, OR, NOT, IN, BETWEEN, ALL, LIKE, ANY
+     Betwise Operators : & (Betwise AND), | (Betwise OR) 
+       
+
+    --  // LIMIT Clause  
+        - return limited data from table 
+        SELECT * FROM STUDENT LIMIT 3;
+
+
+     -- // ORDER BY CLAUSE 
+
+      -return data Ascending and Descending    
+
+      SELECT * FROM STUDENT LIMIT 3 ORDER BY city ASC;
+      SELECT * FROM STUDENT LIMIT 3 ORDER BY city DESC;
+
+
+    --   // Aggregate Function 
+          -> Aggregate Function perform a calculation on a set of values , and return single value
+ 
+          COUNT()
+          MAX()
+          MIN()
+          SUM()
+          AVG()
+
+
+   --    // Group CLause 
+
+         -> group rows that have the same value into summary rows 
+         -> It collect data from multiple records and groups the result by one or more column
+
+          SELECT * FROM Student GROUP BY City;
+
+
+--      // HAVING CLAUSE 
+       -> Similar to where i,e applies some condition on rows.
+       -> Used When we want to apply any condition after grouping.
+
+       Count number of student in each city where max marks cross 90. 
+
+
+      --  Table related Queries 
+
+      Update (to Update exiting rows);
+
+         UPDATE table_name 
+         SET col1 = val1,col2 = val2
+         WHERE conditon ;
+
+
+      --// DELETE 
+
+        (to delete exiting rows)
+
+         DELETE FROM table_name 
+         WHERE conditon;
+
+
+
+   -- //  CASCADING FOR FK
+
+      # On Delete Cascade
+        -> When we create a foreign key using this option, it delete the referencing
+           rows in the child table when the reference row is deleted in the parent table
+           which has a primary key.
+
+      # On Update Cascade
+        ->When we create a foreign key using UPDATE CASCADE the referencing rows are
+         updated in the child table when the reference row is updated in the parent in the
+         parent table which has a primary key. 
+
+
+        /- update and delete automatically when changes in parent child
+
+
+      -- //  Alter  
+
+         Alter 
+         -> to change schema
+
+          ADD Column 
+          DROP column 
+          RENAME column
+          CHANGE COLUMN
+          MODIFY COLUMN
+          DELETE COLUMN
+
+
+   --// JOINS 
+
+       inner join 
+       -> Return Records that have matching values in both tables 
+
+       SELECT column_name(s)
+       FROM table1
+       INNER JOIN table2
+       ON table1.column_name = table2.column_name;
+
+       Left Join 
+       -> returns all records from the left table SELECT column_name(s)
+       FROM table1
+       LEFT JOIN table2
+       ON table1.column_name = table2.column_name;(table1), and the matching records (if any) from the right table (table2).
+
+
+       Right Join 
+       -> returns all records from the right table (table2), and the matching records (if any) from the left table (table1).
+
+       SELECT column_name(s)
+       FROM table1
+       RIGHT JOIN table2
+       ON table1.column_name = table2.column_name;
+
+
+       Full Join 
+      -> returns all records from both tables (table1 and table2).
+
+      SELECT column_name(s)
+      FROM table1
+      CROSS JOIN table2;
+
--- Query.sql
+++ Query.sql
@@ -2,6 +2,7 @@
 
 # Creating DataBase 
    CREATE DATABASE db_name ;
+   CREATE DATABASE IF NOT EXISTS db_name ; (it passed a worning if your database alredy exists)
 
  Ex .
   CREATE DATABASE User ;
@@ -10,6 +11,7 @@
 
 # Drop a Database 
     DROP DATABASE db_name ;
+    DROP DATABASE IF EXISTS db_name ; (it passed a worning if your database Not exists)
 
   Ex.
     drop database Data ; 
@@ -42,7 +44,7 @@ CREATE TABLE table_name (
  );
 
 -- // PRIMARY KEY //
-      -> Primary key is Always Uniqe.
+      -> Primary key is Always Uniqe And Not null.
 
 --    // VARCHAR(45)
      -> For Enter String // (45) store Limit
@@ -59,11 +61,141 @@ CREATE TABLE table_name (
   INSERT INTO Student VALUES (1,"name",13);
 
 
-# Show Table Data 
+-- # Show Table Data 
+  
+    // Show for perticular column
+
+   SELECT colum1 ,column2 FROM table_name;
+
+   // Show all data from table
 
    SELECT * FROM table_name ;
 
    use star(*) for show all table data
 
   
+-- // Foreign Key 
 
+  CREATE TABLE Stduent (
+
+    Cust_id INT ,
+    FOREIGN KEY (Cust_id) references Student(id)
+
+  );
+
+
+  --  // DEFAULT 
+
+   CREATE TABLE Employee (
+    id INT PRIMARY KEY ,
+    Name VARCHAR(30)
+    SALARY INT DEFAULT 25000
+
+   )
+
+
+  --  // CONSTRAINTS 
+      -- CHECK
+
+    CREATE TABLE CONSTRAINT (
+
+    id INT PRIMARY KEY ,
+    Name VARCHAR(30),
+    SALARY INT DEFAULT 25000,
+    age INT,
+    City VARCHAR(50),
+    CONSTRAINT  age_chek CHECK (age >= 18 AND City ="Delhi")
+   );
+
+
+  --  // Operators 
+-- Between
+  SELECT * FROM Student WHERE marks BETWEEN 80 AND 90;
+  -- In
+  SELECT * FROM Student WHERE City IN ("Delhi","Mumbai");
+  --NOT 
+  SELECT * FROM Student WHERE City NOT IN ("Delhi","Mumbai");
+
+
+
+
+  -- // Limite
+   -effect rows 
+   
+    SELECT * FROM Stduent LIMIT 2 ;
+    SELECT * FROM Stduent LIMIT 10 ;
+
+
+    -- //  Aggregate Function 
+
+
+       SELECT MAX(SALARY) FROM EMPLOYEE;
+       SELECT MIN(SALARY) FROM EMPLOYEE;
+       SELECT SUM(SALARY) FROM EMPLOYEE;
+       SELECT AVG(SALARY) FROM EMPLOYEE;
+       SELECT COUNT(SALARY) FROM EMPLOYEE;
+         
+
+   --  // Group Clause
+
+        SELECT SALARY FROM Student GROUP BY City
+
+--   // HAVING CLAUSE 
+
+        SELECT SALARY FROM Student HAVING SALARY > 20000;
+     
+       
+  -- // UPDATE 
+
+       SET SQL_SAFE_UPDATES = 0;
+
+       UPDATE EMPLOYEE SET SALARY = 80000 WHERE SALARY = 25000;
+
+
+  --  // DELETE 
+
+      DELETE FROM EMPLOYEE
+      WHERE name = "Raghav"
+      
+
+--  // CASECADE Foreign 
+
+CREATE TABLE Stduent (
+
+    Cust_id INT ,
+    FOREIGN KEY (Cust_id) references Student(id)
+    ON UPDATE CASCADE
+    ON DELETE CASECADE
+  );  
+
+
+  --  // ALter 
+   
+      #add column
+
+     ALTER TABLE EMPLOYEE 
+     ADD COLUMN age;
+  
+     #DROP column
+     ALTER TABLE EMPLOYEE 
+     DROP COLUMN age ;
+
+     #RENAME Table 
+
+     ALTER TABLE EMPLOYEE 
+     RENAME TO Employee;
+
+      
+     #Change Column 
+
+       ALTER TABLE table_name 
+       CHANGE COLUMN old_name new_name new_datatype new_constraint 
+
+
+     # MODIFY TABLE 
+     ALTER TABLE table_name
+     MODIFY COLUMN column_name datatype;
+
+     #DROP COLUMN
+     ALTER TABLE Persons
+     DROP COLUMN DateOfBirth;
